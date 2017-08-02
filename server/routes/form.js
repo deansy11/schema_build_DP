873fa1dfe009427be04cd9f8b598f491c314e66e
Emily Deans <emilyldeans@gmail.com>
2017-08-02 17:18:13 -0400

    Connected form to root page.

--- server/routes/form.js
+++ server/routes/form.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const data = require("../models/data")
-// const form = require("../views/form")
-
+const data = require("../models/data");
+const mongoose = require("mongoose");
 
 router.get("/", (req, res) => {
-  res.render("form", data);
+  res.render("form", {title : true} );
 });
-//
-// router.post("/", (req, res) => {
-//   console.log(req.body.comment)
-// res.redirect("form");
-// });
+
+router.post("/books", (req, res) => {
+  console.log(req.body.title);
+  res.redirect("form");
+})
+
 
 module.exports = router;
